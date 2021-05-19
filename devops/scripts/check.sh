e946d007d4ea486cbb22344bb40edeebfab3065a
Vital Leshchyk <VitalLeshchyk@gmail.com>
2021-05-20 02:18:24 +0300

    pre-commit

---
+++ devops/scripts/check.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ex
+
+packages=(
+  'argentum_encryption_business'
+  'argentum_encryption_infrastructure'
+  'argentum_standard_business'
+  'argentum_standard_infrastructure'
+  'argentum_user_business'
+  'argentum_user_infrastructure'
+  'demo-app'
+)
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+scriptDir="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+
+for package in ${packages[@]}; do
+  echo $package
+  source "${scriptDir}/check-item.sh" $package
+done
