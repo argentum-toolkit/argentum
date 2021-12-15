53753c049c04a2dfe41aa91e9042acde14bdbf8f
Vital Leshchyk <vitalleshchyk@gmail.com>
2021-12-15 22:34:46 +0300

    Documentation
    Signed-off-by: Vital Leshchyk <vitalleshchyk@gmail.com>

--- devops/scripts/setup.sh
+++ devops/scripts/setup.sh
@@ -6,4 +6,20 @@ ln -s -f ../../devops/scripts/check.sh .git/hooks/pre-commit
 echo "Create folders"
 mkdir -p var/db/user_account/data/
 
+echo "Initialization of Data Base"
+docker-compose stop
+docker-compose up -d service.user.db service.user_account.db
+
+cd argentum_user/infrastructure
+DATABASE_URL=postgres://dev:dev@*:54321/argentum_user diesel migration run
+cd ../..
+
+cd argentum_user_account/infrastructure
+DATABASE_URL=postgres://dev:dev@*:54322/argentum_user_account diesel migration run
+cd ../..
+
+docker-compose stop
+
+echo "Data Base is initialized"
+
 echo "Setup finished"
