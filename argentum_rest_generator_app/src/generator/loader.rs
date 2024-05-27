1ba15f6bdd3ff622df3e61960e981b10ffec3952
Vital Leshchyk <vitalleshchyk@gmail.com>
2024-05-28 00:13:28 +0300

    Example fields for OpenAPI;

--- argentum_rest_generator_app/src/generator/loader.rs
+++ argentum_rest_generator_app/src/generator/loader.rs
@@ -10,11 +10,15 @@ impl OasLoader {
     }
 
     pub fn load(&self, file_path: String) -> (SpecificationRoot, PathBuf) {
-        let path = PathBuf::from(file_path);
+        let path = PathBuf::from(file_path.clone());
 
-        let f = fs::File::open(path.clone()).expect("Should have been able to read the file");
+        let f = fs::File::open(path.clone()).expect(&format!(
+            "Should have been able to read the file {}",
+            file_path
+        ));
 
-        let spec: SpecificationRoot = serde_yaml::from_reader(f).expect("Could not read values.");
+        let spec: SpecificationRoot = serde_yaml::from_reader(f)
+            .expect(&format!("Could not read values from '{}'.", file_path));
 
         (spec, path)
     }
